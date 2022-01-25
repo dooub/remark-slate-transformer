66d9bf813fe15f2659e3f75c048834f57a692da9
Sam Oh <sam@dooub.com>
2022-01-26 03:07:18 +0900

    modify line breaks

--- src/transformers/mdast-to-slate.ts
+++ src/transformers/mdast-to-slate.ts
@@ -23,9 +23,12 @@ function convertNodes(nodes: mdast.Content[], deco: Decoration): slate.Node[] {
   if (nodes.length === 0) {
     return [{ text: "" }];
   }
-
+  let prev_pos = 1;
   return nodes.reduce<slate.Node[]>((acc, node) => {
-    acc.push(...createSlateNode(node, deco));
+    // @ts-ignore
+    acc.push(...new Array(node.position.start.line - prev_pos).fill(createBreak(node)).concat(createSlateNode(node, deco)));
+    // @ts-ignore
+    prev_pos = node.position.end.line;
     return acc;
   }, []);
 }
